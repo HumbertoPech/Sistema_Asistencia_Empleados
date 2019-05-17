ed0cde5f63d23dec83aab469f2ac6923a444f2c8
Wendy Sosa <wendy.p.sosa@gmail.com>
2019-05-16 20:16:23 -0500

    correción rutas requeridas en Conexión
    Se corrigió el nombre del archivo que se requiere,no es variables_BD, si no variables_DB
    Se corrigieron las rutas basado en la estructura de las carpetas

--- core/conexion.php
+++ core/conexion.php
@@ -2,21 +2,22 @@
 class Conexion{
     
     private $conexion_db;
-    private static $defaultInclude = true;
+    private static $defaultInclude = false;
     public function __construct(){
         if(self::$defaultInclude){
-            require_once('config/variables_BD.php');
+            require_once('config/variables_DB.php');
         }else{
-            require_once('../config/variables_BD.php');
+            require_once('../config/variables_DB.php');
         }
         
         $this->conexion_db = new mysqli(DB_HOST, DB_USUARIO, DB_CONTRA, DB_NOMBRE);
-        if($this->conexion_db->connect_errno){
+        if($this->conexion_db->connect_error){
             echo "Fallo al conectar a MySQL: "; //.$this->conexion_db->connect_error;
             return;
         }
         $this->conexion_db->set_charset(DB_CHARSET);
     }
+    
     public function get_conexion(){
         return $this->conexion_db;
     }
