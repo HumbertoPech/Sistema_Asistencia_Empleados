a835f8defe4da2edfe0782045f1b0ec90acdd778
Humberto <humbertopech30@gmail.com>
2019-05-22 02:03:29 -0500

    documentacion

--- libs/operacionesAdministrador.php
+++ libs/operacionesAdministrador.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * @author Humberto Pech 
  * Las variables son pasadas mediante la variable global $_POST[] por el navegador,
  * entre estas variables se encuentra una llamada 'operacion', esta define que función es requerida
  * para generar una salida en formato JSON, que será leído por el navegador (mediante AJAX)
@@ -31,10 +32,13 @@ if(isset($_POST['operacion'])){
  * la misma que se encuentra en la BD.
  */
 function comprobarContrasena(){
+    //requires
     require_once("../config/config.php");
     require_once(LIBS_PATH."consultas.php");
+    //datos recibidos
+    //TODO checar si la contraseña esta encriptada
     $contrasenaRecibida = $_POST['contrasenaAdmin'];
-    //ver si se encripta la contraseña
+
     $sql = "SELECT contrasena FROM administrador WHERE id = 1";
     $contrasenaAdmin=consultar($sql)['contrasena'];
     $resultado['resultado'] = false;
@@ -49,9 +53,12 @@ function comprobarContrasena(){
  * filas actualizadas en la bd.
  */
 function resetIntentosUsuario(){
+    //requires
     require_once("../config/config.php");
     require_once(LIBS_PATH."consultas.php");
+    //datos recibidos
     $id_usuario = $_POST['id_usuario'];
+
     $sql = "UPDATE empleado_intentos SET numero_intentos = 0 WHERE id_empleado =" . $id_usuario;
     json_encode(actualizar($sql));
 }
@@ -61,10 +68,13 @@ function resetIntentosUsuario(){
  * Devuelve las filas afectadas.
  */
 function cambiarEstadoEmpleado(){
+    //requires
     require_once("../config/config.php");
     require_once(LIBS_PATH."consultas.php");
+    //datos recibidos
     $id_usuario = $_POST['id_usuario'];
     $estado_nuevo = $_POST['estado_nuevo'];
+
     $sql = "UPDATE empleados SET id_estado = (SELECT id FROM estados WHERE nombre = '$estado_nuevo') WHERE id =".$id_usuario;
     json_encode(actualizar($sql));
 }
@@ -74,8 +84,10 @@ function cambiarEstadoEmpleado(){
  * Devuelve las filas afectadas.
  */
 function actualizarInformacionEmpleado(){
+    //requires
     require_once("../config/config.php");
     require_once(LIBS_PATH."consultas.php");
+    //datos recibidos
     $id_usuario = $_POST['id_usuario'];
     $nombres = $_POST['nombres'];
     $apellidos = $_POST['apellidos'];
@@ -86,6 +98,7 @@ function actualizarInformacionEmpleado(){
     $estado_civil = $_POST['estado_civil'];
     $curp = $_POST['curp'];
     $fecha_inicio = $_POST['fecha_inicio'];
+
     $sql = "UPDATE empleados SET nombres = '$nombres', apellidos = '$apellidos', sueldo_base = '$sueldo_base', direccion = '$direccion',".
     "fecha_nacimiento = '$fecha_nacimiento', sexo = '$sexo', estado_civil = '$estado_civil', curp = '$curp', fecha_inicio = '$fecha_inicio' WHERE id = ".$id_usuario;
     json_encode(actualizar($sql));
@@ -96,12 +109,15 @@ function actualizarInformacionEmpleado(){
  * Devuelve el número de filas afectadas.
  */
 function actualizarHorarioEmpleado(){
+    //requires
     require_once("../config/config.php");
     require_once(LIBS_PATH."consultas.php");
+    //datos recibidos
     $id_usuario = $_POST['id_usuario'];
     $dia = $_POST['dia'];
     $hora_entrada = $_POST['hora_entrada'];
     $hora_salida = $_POST['hora_salida'];
+
     $sql = "UPDATE horarios SET hora_entrada = '$hora_entrada', hora_salida = '$hora_salida' WHERE id_empleado = ". $id_usuario ." AND dia = '$dia'";
     json_encode(actualizar($sql));      
 }
