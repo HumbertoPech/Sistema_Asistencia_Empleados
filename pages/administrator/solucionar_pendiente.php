cf277797fcdefa4cd185978bd558d9fc05d42867
Ulises Ancona <uliancona@hotmail.com>
2019-05-22 16:01:12 -0500

    Add files via upload

--- pages/administrator/solucionar_pendiente.php
+++ pages/administrator/solucionar_pendiente.php
@@ -12,18 +12,27 @@ if(isset($_POST['id'])){
     $id = $_POST['id'];
 }
 if(isset($_POST["dato_final"])){
+    
     $dato_final = $_POST['dato_final'];
     $tipo = $_POST['tipo'];
     $descripcion = $_POST['descripcion'];
     $id = $_POST['id'];
+    var_dump($descripcion);
 
     $query = "INSERT into solucion_pendientes (id_trabajo_diario, dato_original, dato_final, tipo, descripcion) values ($id, null,null, '$tipo', '$descripcion')";
+    $query = "INSERT into 'solucion_pendientes' ('id_trabajo_diario', 'dato_original', 'dato_final', 'tipo', 'descripcion') values (3, '05:00:00','22:00:00', 'E', 'mi mama')";
+
+    $query = "INSERT INTO `solucion_pendientes` (`id`, `id_trabajo_diario`, `dato_original`, `dato_final`, `tipo`, `descripcion`) VALUES (NULL, '2', '09:00:00', '19:00:00', 'E', 'SDFSDF')";
+    //$query = "INSERT INTO 'trabajo_diario' ('id', 'id_empleado', 'en_nomina', 'hora_entrada', 'hora_salida', 'fecha', 'horas_trabajadas') VALUES (NULL, '1', '', '05:00:00', '22:00:00', '2019-05-20', NULL)";
     if($conexion){
         $resultado = $conexion->query($query);
+        var_dump($resultado);
         $query2 = "UPDATE trabajo_diario SET hora_salida ='$dato_final' where id = $id  ";
         $resultado2 = $conexion->query($query2);
     }
 }
+
+
 ?>
 <?php include("../../resources/html/header_admin_empleados.html");?>
 <script type="text/javascript" src="../../resources/js/funciones_solucionar_pendientes.js"></script>
@@ -34,7 +43,7 @@ if(isset($_POST["dato_final"])){
         <div class="col-md-6 mx-auto">
             <div class="card card-body">
                 <form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
-                    <p hidden id="hora_ini">Dato original:></p>
+                    <p hidden id="hora_ini">Dato original:</p>
                     <input type="hidden" id="id" name="id" value=<?php$id?>>
                     <p>Dato final: <input type="time" name = "dato_final" id="dato_final" min="08:00" max="20:00" step="600" required></p>
                     <label>Tipo de pendiente:</label>
